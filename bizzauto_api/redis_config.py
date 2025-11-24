696564a9a05924f61e302bd4eaa3737351a4955a
munibaweb123 <munibavirtualuni@gmail.com>
2025-11-24 08:33:10 +0500

    Refactor: Use REDIS_URL variable in redis_config.py

--- bizzauto_api/redis_config.py
+++ bizzauto_api/redis_config.py
@@ -1,8 +1,10 @@
-import redis
 import os
+import redis
 from rq import Queue
 
-redis_conn = redis.from_url(os.environ.get("REDIS_URL", "redis://localhost:6379/0"), decode_responses=True)
+REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
+
+redis_conn = redis.from_url(REDIS_URL, decode_responses=True)
 
 # Create a default queue
 # You can create multiple queues for different types of jobs
